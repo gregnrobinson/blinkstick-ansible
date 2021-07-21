3f406df8ac0f06ebc5225af00a6c3218a6d6ef1f
Greg Robinson <gregnrobinson@gmail.com>
2021-07-21 01:55:24 -0400

    cleanup

--- crons/jobs/night.sh
+++ crons/jobs/night.sh
@@ -1,10 +1,3 @@
-# BEGIN ANSIBLE MANAGED BLOCK
- #!/bin/bash
+#!/bin/bash
 
-# add ansible paths for project
-export BLINK_PATH="/home/pi/blinkstick-ansible"
-export ANSIBLE_CONFIG="/home/pi/blinkstick-ansible/ansible.cfg"
-
-# blink-night
-ansible-playbook ${BLINK_PATH}/main.yaml -i ${BLINK_PATH}/inventory/all.yaml -t blink-night
-# END ANSIBLE MANAGED BLOCK
+sudo blink-night
